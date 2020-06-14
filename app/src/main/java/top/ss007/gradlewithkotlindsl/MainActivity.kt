6530e590b03d4054108ed80ba2fad2b20eb32838
shusheng007 <568894336@qq.com>
2020-06-14 23:49:50 +0800

    first commit

---
+++ app/src/main/java/top/ss007/gradlewithkotlindsl/MainActivity.kt
@@ -0,0 +1,11 @@
+package top.ss007.gradlewithkotlindsl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
