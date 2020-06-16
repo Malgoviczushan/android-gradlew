24be49beab1658ce0f8485a954d66083aad66a51
shusheng007 <568894336@qq.com>
2020-06-16 23:39:19 +0800

    feature: use buildSrc for unified versioning

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,12 +1,21 @@
 object Config {
     object Android {
-        val buildToolsVersion = "30.0.0"
-        val minSdkVersion = 21
-        val targetSdkVersion = 29
-        val compileSdkVersion = 29
-        val applicationId = "top.ss007.gradlewithkotlindsl"
-        val versionCode = 1
-        val versionName = "1.0"
+        const val buildToolsVersion = "30.0.0"
+        const val minSdkVersion = 21
+        const val targetSdkVersion = 29
+        const val compileSdkVersion = 29
+        const val applicationId = "top.ss007.gradlewithkotlindsl"
+        const val versionCode = 1
+        const val versionName = "1.0"
+    }
+
+    object AndroidLib{
+        const val appcompatVersion= "1.1.0"
+        const val constraintLayoutVersion="1.1.3"
+    }
+
+    object Kotlin{
+        const val kotlinVersion="1.3.72"
     }
 }
 
